faf66a2e45b025d313c85f49662131d59a17dbb3
Dova81 <33400774+Dova81@users.noreply.github.com>
2019-05-15 18:54:30 -0300

    Kinda works

--- dios/src/Components/PokemonsComponent/pokemonComponent.jsx
+++ dios/src/Components/PokemonsComponent/pokemonComponent.jsx
@@ -4,7 +4,22 @@ import React, { Component } from 'react';
 
 class PokemonComponent extends Component {
     
+  constructor(props){
+    super(props);
+    const pokemon =this.props.location.state.pokemon
+    this.state = {
+     idPoke:pokemon.id,
+     nombre:pokemon.especie.nombre,
+     tipo:pokemon.especie.tipo,
+     victorias:pokemon.victorias,
+     fechaCaptura:pokemon.fechaCaptura,
+
+  } 
+  }
+
   render() {
+    console.log(this.state);
+    
     
     return (
       
--- dios/src/Components/PrimerDivMedio.js
+++ dios/src/Components/PrimerDivMedio.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
-import {withRouter} from "react-router-dom";
+
+import {withRouter, Link} from "react-router-dom";
 import "./Css/PrimerDiv.css"
 
 
 class PrimerDivMedio extends Component {
 
   dameFotos (bichos){
-    return bichos.map(b => <img onClick={this.verStats} src={b.especie.urlFoto} alt={b.especie.nombre} className="ajustarGim" />)
+    return bichos.map(b => 
+      <Link to={{
+        pathname:'/entrenador/pokemon',
+        state:{
+          pokemon:b
+        }
+      }}>
+      
+    <img src={b.especie.urlFoto} alt={b.especie.nombre} className="ajustarGim" />
+    </Link>
+  )
   }
 
- verStats =() =>{
-  this.props.history.push("/entrenador/pokemon")   
- }
     
   render() {
     
